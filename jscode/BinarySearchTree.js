7cfbf03a3ed9ce37b35398338d84fa68806c39e9
annilq <410493517@qq.com>
2017-12-01 15:22:50 +0800

    update

--- jscode/BinarySearchTree.js
+++ jscode/BinarySearchTree.js
@@ -100,9 +100,50 @@ class BinarySearchTree {
       }
       return current.key;
     }
-    return null
+    return null;
   }
-  remove(key) {}
+  remove(key) {
+    this.root = this._removeNode(this.root, key);
+  }
+  _removeNode(node, key) {
+    if (node === null) {
+      return null;
+    }
+    if (node.key > key) {
+      node.left = this._removeNode(node.left, key);
+      return node;
+    } else if (node.key < key) {
+      node.right = this._removeNode(node.right, key);
+      return node;
+    }
+    // 执行到这里就说明找到了要删除的节点,需要判断三种情况
+    // 1. 没有子节点
+    // 2. 只有一个节点
+    // 3. 有两个节点
+    // -------------------------------------
+    // 没有子节点
+    if (node.left === null && node.right === null) {
+      node = null;
+      return node;
+    }
+    // 有一个节点
+    if (node.left === null) {
+      return node.right;
+    } else if (node.right === null) {
+      return node.left;
+    }
+    // 有两个节点
+    let minNode = this._findMinNode(node);
+    node.key = minNode.key;
+    node.right = this._removeNode(node.right, minNode.key);
+    return node;
+  }
+  _findMinNode(node) {
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
+  }
 }
 
 let tree = new BinarySearchTree();
@@ -115,8 +156,11 @@ console.log(tree.search(5));
 console.log(tree.min());
 console.log(tree.max());
 console.log("-----------------inOrderTraverse------------");
-console.log(tree.inOrderTraverse());
+tree.inOrderTraverse();
 console.log("-----------------preOrderTraverse------------");
-console.log(tree.preOrderTraverse());
+tree.preOrderTraverse();
 console.log("-----------------postOrderTraverse------------");
-console.log(tree.postOrderTraverse());
+tree.postOrderTraverse();
+console.log("-----------------remove------------");
+tree.remove(4);
+tree.inOrderTraverse();
