be643f656bd3ca807d75d89ca859f814b8395117
annilq <410493517@qq.com>
2022-07-15 11:44:43 +0800

    update

--- public/scripts/errorhandle.js
+++ public/scripts/errorhandle.js
@@ -1,33 +1,10 @@
 window.onerror = function (msg, url, line, col, error) {
-  // Note that col & error are new to the HTML 5 spec and may not be 
-  // supported in every browser.  It worked for me in Chrome.
   var extra = !col ? '' : '\ncolumn: ' + col;
   extra += !error ? '' : '\nerror: ' + error;
-
-  // You can view the information in an alert to see things working like this:
+  // 全局捕获，避免白屏
   console.error("Error: " + msg + "\nurl: " + url + "\nline: " + line + extra);
 
-  // TODO: Report this error via ajax so you can keep track
-  //       of what pages have JS issues
-
   var suppressErrorAlert = true;
-  // If you return true, then error alerts (like in older versions of 
-  // Internet Explorer) will be suppressed.
+
   return suppressErrorAlert;
 };
-
-// if (NativeUtil) {
-//   NativeUtil.use("close");
-// }
-
-
-function slowdown() {
-  var seconds = 0.5
-  var start = (new Date()).getTime()
-  var end = start
-  while (end - start < seconds * 1000) {
-    end = (new Date()).getTime()
-  }
-}
-
-setInterval(slowdown,1000)
