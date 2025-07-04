7a9b00ee03c8251e97558dcf835e04bd41255008
annilq <410493517@qq.com>
2025-07-04 14:53:46 +0800

    update

--- app/thoughts/page.tsx
+++ app/thoughts/page.tsx
@@ -1,40 +1,36 @@
-import { format } from "date-fns"
+import { format } from "date-fns";
 
 import Layout from "@/components/layout";
 import { Card } from "@mui/joy";
 import { addThought, getThoughts } from "./actions";
-import ThoughtForm from "./ThoughtForm"
+import ThoughtForm from "./ThoughtForm";
 import ThoughtDelBtn from "./ThoughtDelBtn";
-import { auth } from "@/auth"
+import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
 export default async function Thoughts() {
-  const session = await auth()
-  if (!session?.user) {
-    redirect("/")
-  }
-  const thoughts = await getThoughts()
+  const session = await auth();
+  const thoughts = await getThoughts();
 
   return (
     <Layout>
       <div className="container mx-auto">
         <div className="space-y-4">
-          <ThoughtForm addThought={addThought} />
+          {session?.user && <ThoughtForm addThought={addThought} />}
 
           {thoughts.map((thought) => (
             <Card variant="soft" className="flex flex-col" key={thought.id}>
-              <div className="whitespace-pre-wrap">
-                {thought.content}
-              </div>
+              <div className="whitespace-pre-wrap">{thought.content}</div>
               <div className="flex justify-between text-sm text-foreground">
-                <time dateTime={thought.createdAt.toISOString()}>{format(thought.createdAt, "yyyy-MM-dd HH:mm:ss")}</time>
-                <ThoughtDelBtn id={thought.id} />
+                <time dateTime={thought.createdAt.toISOString()}>
+                  {format(thought.createdAt, "yyyy-MM-dd HH:mm:ss")}
+                </time>
+                {session?.user && <ThoughtDelBtn id={thought.id} />}
               </div>
             </Card>
           ))}
         </div>
       </div>
     </Layout>
-  )
+  );
 }
-
