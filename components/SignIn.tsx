a5564d9e55ca13a519c27b32e5a24f4d36303e87
annilq <410493517@qq.com>
2026-01-07 11:02:35 +0800

    update

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,29 +1,29 @@
-'use client';
+"use client";
 
-import { Button, Dropdown, Menu, MenuButton, MenuItem } from '@mui/joy';
-import { LogInIcon, LogOutIcon, UserRound } from 'lucide-react';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { Button, Dropdown, Menu, MenuButton, MenuItem } from "@mui/joy";
+import { LogInIcon, LogOutIcon, UserRound } from "lucide-react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function SignIn() {
-  const session = useSession()
+  const session = useSession();
 
   if (session.data?.user) {
-
     return (
       <Dropdown>
-        <MenuButton variant="plain" ><UserRound /></MenuButton>
+        <MenuButton variant="plain">
+          <UserRound />
+        </MenuButton>
         <Menu>
-          <MenuItem onClick={() => signOut()}><LogOutIcon /></MenuItem>
+          <MenuItem onClick={() => signOut()}>
+            <LogOutIcon />
+          </MenuItem>
         </Menu>
       </Dropdown>
-    )
+    );
   }
   return (
     <div className="space-y-1">
-      <Button
-        onClick={() => signIn('github')}
-        variant="plain"
-      >
+      <Button onClick={() => signIn("github")} variant="plain">
         <LogInIcon />
       </Button>
     </div>
